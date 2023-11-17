bdd82e6063b6f17000c77bd22a3d0a322f1106ea
khasanrabbimov <145132898+khasanrabbimov@users.noreply.github.com>
2023-11-17 16:46:47 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,230 @@
+/*77//////////////////////////////////////////////////////////////////////////////////////////////////////////++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\/* 
+
+let txt = "Lorem ipsum,  dolor sit amet consectetur adipisicing elit. Ab corporis inventore ratione voluptatum, delectus culpa mollitia natus consectetur dolore aperiam, nihil unde non laudantium assumenda, dolores temporibus. In, consequatur sit"
+ */
+/* console.log(
+  str.slice(str.search("h"), str.search("h") + 1)  +
+  txt.slice(str.search("a"), str.search("a") + 1) +
+    txt.slice(str.search("s"), str.search("s") + 1) +
+    txt.slice(str.search("a"), str.search("a") + 1) +
+    txt.slice(str.search("n"), str.search("n") + 1)  
+
+  ); */
+      
+
+/*   let str = " JOHN SAID HELLO WORLD hjbbkjknvjvdjfjhvhv"
+  
+console.log( */
+/*    str.toUpperCase(),
+  str.toLowerCase(),  
+
+
+   str.slice(16, 21) */
+ 
+
+/*  str.search('D') */  // находит элемент и показывает его индекс
+/* str.slice(str.search("WORLD"),  str.search('D') + 1 )
+
+
+
+);
+    */
+
+
+/* bknbnngjnsmnnjvjjjnnmcnnvn
+kofkmfn vnjkfknv */
+/* 
+ let num = "ты чмо Шахбоз"
+console.log(num); */
+
+/* alert() 
+prompt() - /* для ввода  */
+/* confirm() */
+
+/* 
+parseInt(num); // "10.55" > 10
+//parseFloat(num) ; // " 10.55" > 10.55
+let num = '10.55'
+Math.ceil()
+Math.floor()
+Math.round()
+Math.max()
+Math.min()
+Math.random()
+num.toFixed()
+num.toString()
++num - "55" => 55 */
+/* console.log() */
+
+
+/* let password = "password123"
+
+let psw = prompt('пароль')
+if(password = "password123") {
+  alert(true)
+}else(
+  alert(error)
+) */
+
+
+
+
+// let str = "Alex"
+/* alert('hello world')
+console.log('hi');
+console.log('changes');
+ */
+
+// str.slice(ot, do)
+// str.toUpperCase()
+// str.toLowerCase()
+// str.search('ll') // 2
+// str.trim() // --> " dsd  " <--
+// str.includes("llo") // true
+// str.replace("o", "a") // 
+// str.replaceAll("o", "a") // 
+// str.charAt()  // 
+// str[0] // 
+// let arr = ['hasan', 'shoxa', 'jaxongir']
+
+// arr[99] = "bexruz"
+
+// console.log(arr.length); // 3 | 4
+
+// types
+// object
+// number
+// string
+// boolean || true false
+// undefined
+// NaN // number
+// null // object
+
+// % // 
+// 7 / 2 = 3.5
+// 7 % 2 = 1
+// /
+// 2 ** 7
+
+
+// !== не равенство (строгое)
+// != не равенство (не строгое)
+
+
+
+
+
+//Math.round(num)
+/* Math.floor(num)
+Math.ceil(num)
+num,
+parseInt(num) /* string -> number "22.90" -> 22 */
+/* parseFloat(num) //string -> number "22.90" -> 22.90
++num 
+Math.floor(math.random() * 10)
+Math.max(num, num.second, 1234, 123324, 12344334, 12322134)
+Math.min(num, 123, 322, 454, 322)
+Math.pow(2, 7) */
+/* 
+let answer = confirm('are you 18?')
+if (answer ===true) {
+  alert('Welcome') 
+}else{
+  alert('poshel von')
+}
+
+let age = +prompt('How old are you?') 
+if(age >= 18) {
+  alert('Welcome')
+}else{
+  alert('go home')
+}
+ */
+
+/* let list = 'putin shavkat alibek biden zelenskiy'
+let name = prompt('what is your name?').toLowerCase().trim()
+if(list.includes(name)) {
+  console.log('welcome');
+}else{
+  console.log('goodbye');
+}
+ */
+
+
+/* 
+
+let name =  +prompt('Как вас зовут?')
+if(name == 'Alex' ) {
+alert('Удачно')
+}else{
+  alert('Пользователь не найден')
+}
+
+let money = +prompt('skolko obnalichit?')
+if(money <= 10000 ) {
+  alert('Обналичить')
+}else{
+  alert('Недостаточно средств')
+}
+
+let account = +prompt('nomer cheta?')
+if(account === 7777 ) {
+  alert('Пройдите')
+}else{
+  alert('Ошибка!')
+}
+
+
+ */
+/* 
+let a = prompt('a')
+let m = prompt('+ - * /')
+let b = prompt('b')
+if(m === '+') {
+  alert('a + b')
+}else if(m === '-') {
+  alert(' a - b')
+}else if(m === '*') {
+  alert(' a * b')
+}else if(m === '/') {
+  alert('/')
+}
+ */
+/* 
+
+let age = prompt('how old are u?')
+if(age  > 1 &&  age  < 10) {
+  console.log('child');
+}else if(age > 10 && age < 20) {
+  console.log('teeneger');
+}else if(age > 20 && age  < 30) {
+   console.log('grandpa'); console.log('time to marry');
+}else if(age  > 30 && age   < 35){
+  console.log('Пора завести детей');
+}else if(age  > 40  && age  < 45) {
+  console.log('Пора детей на работу');
+}else if(age  > 45 && age   < 50) {
+  console.log('Пора на пенсию');
+}else{
+
+} */
+
+
+
+/*  
+let name = prompt('name').toLowerCase().trim()
+if(name = [0] === 'a') {
+  console.log('welcome');
+}else{
+  console.log('goodbye'); {
+    let password = prompt('password')
+    if(password === 7777) {
+      console.log('welcome');
+    }else{
+      console.log('goodbye');
+    }
+  }
+}  */
+
+
+let name = prompt('what is ur name?')
