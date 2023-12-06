5cda689aecf70f0b3df78b6149ab398862e4a97f
khasanrabbimov <145132898+khasanrabbimov@users.noreply.github.com>
2023-12-06 16:35:29 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ console.log(boy.name, boy.surname, boy.birthdate, boy.himself.class, boy.himself
 
 
 
-let car = [ 
+let cars = [ 
     {
       
             model: 'Rolce Royc Mansory',
@@ -133,21 +133,145 @@ let car = [
 ]
 
 
-let cars_price = [100000, 500000, 145000, 37000,  20000, 10000,  40000, 14000,  ]
-let upto = prompt('на какую машину u вас хватает денег?')
-let filtered = cars_price((item) => {
-    if(item.price >= cars_price) {
-        console.log(item);
+// let cars_price = [100000, 500000, 145000, 37000,  20000, 10000,  40000, 14000,  ]
+// let upto = prompt('на какую машину u вас хватает денег?')
+// let filtered = cars_price((item) => {
+//     if(item.price >= cars_price) {
+//         console.log(item);
+//     }
+// })
+// console.log(
+//     'По вашему запросу найдено ${filtered.length} машин ${filtered}'
+// );
+// console.log(cars_price);
+//    let year = cars.filter(item => {
+//      if (item.Details.Year >= 2020) {
+//         console.log(item);
+//      }
+//        })
+//  */
+/* let boy = {
+name: 'Abdumannon',
+surname: 'Ilhomov',
+birthdate: '26 march, 2010',
+weigh:  '45kg',
+heigh: '158sm',
+himself: {
+    child: '1st child',
+    characteristic: 'charismatic',
+    school: '27',
+    class: '7b',
+    
+}
+}
+console.log(boy.name, boy.surname, boy.birthdate, boy.himself.class, boy.himself.child); */
+
+
+
+
+
+
+
+/* let people = [
+    {
+        name: 'Al Safir',
+        surname: 'Bozorov',
+        birth_date:'20 December 2008',
+weigh: '55kg',
+heigh: '170 sm',
+country: "UZB",
+city: 'Samarkand',
+himself: {
+child: 'first child',
+characteristic: 'curious',
+school: 24,
+class: '9z'
+}
+
+    },
+]
+
+
+let second_person = [
+    
+    {
+        name: "Shaxboz",
+        surname: 'Aminov',
+        birth_date:'27 June 2008',
+        weigh: '60kg',
+        heigh: '175 sm',
+        country: "UZB",
+        city: 'Samarkand',
+        himself: {
+            child: 'second child',
+    characteristic: 'charismatic',
+    school: 29,
+    class: '9a'
+}
+
+}
+
+
+]
+Object.assign({}, people, second_person);
+Object.freeze(people);
+Object.keys(people, [0]);
+Object.keys(second_person, [0] );
+Object.values(people);
+Object.values(second_person);
+concat(people);
+concat(second_person);
+console.log(people, second_person);
+
+
+ */
+
+/* let types = {
+    string: [],
+    number:[],
+    object: [],
+    boolean: []
+}
+
+
+
+
+let new_object = Object.assign({}, people, second_person)
+let keys = Object.keys(new_object)
+let values = Object.values(new_object)
+let arr = keys.concat(values)
+console.log(arr);
+
+arr.filter((item) => {
+    if(item => typeof item === 'string') {
+        types.string.push(item)
+    }else if(item => typeof item === 'number') {
+        types.string.push(item)
+    }else if(item => typeof item === 'object') {
+        types.string.push(item)
+    }else{
+        types.boolean.push(item)
     }
 })
-console.log(
-    'По вашему запросу найдено ${filtered.length} машин ${filtered}'
-);
-console.log(cars_price);
-   let year = cars.filter(item => {
-     if (item.Details.Year >= 2020) {
-        console.log(item);
-     }
-       })
+console.log(arr, types);
+ */
 
- 
+
+let discount = prompt("Сколько сделать скидку")
+
+for(let item of cars) {
+    let atveti = item.price - ((item.price / 100) * discount)
+
+    console.log(item.model);
+    console.log(atveti);
+}
+
+
+
+
+
+
+
+
+
+
